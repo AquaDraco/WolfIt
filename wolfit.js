4d3cada0502b8becb947cbae4c0f7ef534a6d3c4
AquaDraco <aquadracon@gmail.com>
2018-12-22 15:08:25 -0500

    Update wolfit.js

--- wolfit.js
+++ wolfit.js
@@ -1,1 +1,21 @@
-<
+$(document).ready(function() {
+});
+
+function wolfit() {
+  var suffix = $("#suffix").val();
+  
+  if((suffix != null) && (suffix != "")) {
+    $.ajax({
+      method: "GET",
+      url: "https://www.googleapis.com/customsearch/v1",
+      data: JSON.stringify(
+        {
+          key: "AIzaSyAPNdD37QRurD7oLMRYJbMxsKuersRpwUA",
+          cx: "012397244432696523068:-veiaxy5zx8",
+          q: suffix,
+          searchType: "image"
+        }
+      );
+    });
+  }
+}
